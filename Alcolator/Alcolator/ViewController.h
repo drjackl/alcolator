73cdd5e7d158204d1a3fca5cce0ede9fae4f6e6f
Jack Li <drjackl@gmail.com>
2016-02-20 17:59:56 -0800

    Finished assignment 19. Trying a direct commit -am

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Jack Li on 2/20/16.
+//  Copyright © 2016 Jack Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
